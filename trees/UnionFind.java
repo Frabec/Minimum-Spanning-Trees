d9d78f4ad1ac4722bc66f89cc098d092d3ea4ef6
Franco Rabec <franco.rabec@hotmail.fr>
2017-12-19 16:58:18 +0100

    Implements union find

--- trees/UnionFind.java
+++ trees/UnionFind.java
@@ -7,20 +7,32 @@ import java.util.HashMap;
 // Q1
 
 public class UnionFind {
-	//parent relation, parent.put(src,dst) indicates that src points to dst
-    private HashMap<Place,Place> parent;
-    
-    public UnionFind( ){
-        //TODO
-    }
-    
-    public Place find( Place src ){
-        //TODO
-    	return null;
-    }
-    
-    public void union( Place v0, Place v1 ){
-        //TODO
-    	return;
-    }
+	// parent relation, parent.put(src,dst) indicates that src points to dst
+	private HashMap<Place, Place> parent;
+
+	public UnionFind() {
+		this.parent = new HashMap<Place, Place>();
+	}
+
+	public Place find(Place src) {
+		Place father = this.parent.get(src);
+		Place root;
+		if (father == null) {
+			return src;
+		}
+		
+		root = find(father);
+		parent.put(src, root);
+		return (root);
+
+	}
+
+	public void union(Place v0, Place v1) {
+
+		if (find(v0).equals(find(v1))) {
+			return;
+		}
+
+		parent.put(find(v1), find(v0));
+	}
 }
